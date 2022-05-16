2aeed3e244aaca9b6a7b73bd73f3e52057a33cdb
Sumit K <synergy2411@outlook.com>
2022-05-16 16:35:20 +0530

    nested queries

--- 01-graphql-basics/src/index.js
+++ 01-graphql-basics/src/index.js
@@ -6,11 +6,26 @@ const users = [
     {id: "103", name : "jenny", email:"jenny@test", age: 35},
 ]
 
+const posts = [
+    { id: "201", title : "GraphQL 101", body : "Good post", published : false, author: "101"},
+    { id: "202", title : "GraphQL for Beginners", body : "Nice post", published : true,  author: "101"},
+    { id: "203", title : "GraphQL Mastering", body : "I Like it", published : false,  author: "102"},
+    { id: "204", title : "GraphQL for All", body : "Awesome Course", published : true,  author: "103"},
+]
+
 // Structure
 const typeDefs = gql`
     type Query {
         me: User! 
-        users(query: String): [User!]!
+        users(query: String, min: Int): [User!]!
+        posts(search:String): [Post!]!
+    }
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
     }
     type User {
         id:ID!
@@ -23,9 +38,22 @@ const typeDefs = gql`
 // Behaviour
 const resolvers = {
     Query : {
-        users(parent, args, ctx, info){
+        posts(parent, args, ctx, info){
+            if(args.search){
+                return posts.filter(post=> {
+                    const titleMatch = post.title.toLowerCase().includes(args.search.toLowerCase())
+                    const bodyMatch = post.body.toLowerCase().includes(args.search.toLowerCase())
+                    return titleMatch || bodyMatch
+                })
+            }
+            return posts;
+        },
+        users(parent, args, context, info){
             if(args.query){
                 return users.filter(user => user.name.toLowerCase().includes(args.query.toLowerCase()))
+            }
+            if(args.min){
+                return users.filter(user => user.age >= args.min)
             }
             return users;
         },
@@ -35,6 +63,12 @@ const resolvers = {
                 name : "Alice Doe",
                 email: "alice@test.com"
             }
+        }
+    },
+    Post : {
+        author(parent, args, ctx, info){
+           const userFound =  users.find(user => user.id === parent.author)
+           return userFound;
         }
     }
 }
